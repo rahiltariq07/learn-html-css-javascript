d4cde9403ad5be62f6764cfafe515cf8a5ce618d
rahiltariq07 <163430192+rahiltariq07@users.noreply.github.com>
2024-04-15 19:04:47 +0530

    day2-part1

---
+++ Learn JS/Day 2 (part I)/script.js
@@ -0,0 +1,63 @@
+console.log("Hello Worlds");
+
+let age = 0;
+let grace = 2;
+
+// Airthematic Operators
+
+console.log(age + grace); // Addition
+console.log(age - grace); // Subtraction
+console.log(age * grace); // Multiplication
+console.log(age / grace); // Divide
+console.log(age ** grace); // Exponential (5 raise to the power 2)
+console.log(age % grace); // Remainder
+
+// if condition
+
+if(age>18) {
+    console.log("You can Drive");
+}
+
+// if else if condition
+
+else if(age == 0) {
+    console.log("Are you Kidding??")
+}
+
+// if else condition
+
+else {
+    console.log("You Cannot Drive");
+}
+
+
+
+// Assignment Operators
+
+/* age += grace
+console.log(age); // now age becomes age+grace */
+
+/* age -= grace 
+console.log(age); // now age becomes age-grace */
+
+/*same with multiplication, division, exponential & remaider*/
+
+// Comparison Operator
+
+/* == (equal to)
+   != (not equal)
+   === (equal value & type)
+   !== (not equal value or not equal type)
+   > (greater than)
+   < (less than)
+   >= (greater than or equal to)
+   <= (less than or equal to)
+   ? (ternary operator) */
+// Logical Operator
+/* && (logical and) overall statement
+   || (logical or) one statement
+   ! (logical not) */
+
+// Java Script Ternary Operator
+
+/* ? [ eg: a=6, b=8, c= a>b ? (a-b) : (b-a) ] (( here ? is if and : is else )) */
