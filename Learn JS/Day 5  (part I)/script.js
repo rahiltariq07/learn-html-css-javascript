fa240eecaef8e7353ea06c5338a341a2c639386f
rahiltariq07 <rahiltariq07@gmail.com>
2024-04-26 10:14:54 +0530

    day-5

--- Learn JS/Day 5  (part I)/script.js
+++ Learn JS/Day 5  (part I)/script.js
@@ -1,5 +1,42 @@
-//For Loop
+// For Loop
 
-for (let i = 1; i <= 5; i++) {
-    console.log("i=", i);
-}
+// for (let i = 1; i <= 5; i++) {
+//     console.log("i=", i);
+// }
+
+// Whie Loop
+
+// let i = 1;
+// while (i<=5) {
+//     console.log("i=" , i);
+//     i++;
+// }
+
+// Do-While Loop
+
+// let i = 1;
+// do {
+//     console.log("i=", i);
+//     i++;
+// } while (i<=5);
+
+// For-Of Loop
+
+// let str = "Rahil";
+
+// for (let i of str) {
+//     console.log("i=", i);
+// }
+
+// For-In Loop
+
+// let student = {
+//     name: "Rahil Tariq",
+//     age: "20",
+//     cgpa: "7.5",
+//     isPass: true
+// };
+
+// for (let key in student) {
+//     console.log(key, student[key]);
+// }
