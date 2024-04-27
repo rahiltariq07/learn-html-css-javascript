c78ffead33fb7cc4ec55c5c3ebaff7775d343ecf
rahiltariq07 <rahiltariq07@gmail.com>
2024-04-27 10:43:06 +0530

    day-6

--- Learn JS/Day 6 (part II)/script.js
+++ Learn JS/Day 6 (part II)/script.js
@@ -1,7 +1,22 @@
 // String Methods
 let str = "Rahil Tariq";
+
 console.log(str.toUpperCase());
+
 console.log(str.toLowerCase());
 
-let str2 = "     Rahil     is    a   Programmer      ";
-console.log(str2.trim());  // removes starting and ending spaces, not middle spaces
+let str0 = "     Rahil     is    a   Programmer      ";
+console.log(str0.trim()); // removes starting and ending spaces, not middle spaces
+
+console.log(str.slice(1, 9));
+
+let str1 = "Rahil";
+let str2 = "Tariq";
+let result = str1.concat(str2); // we can also write str1 + str2
+console.log(result);
+
+let str3 = "hellololololo";
+console.log(str3.replace("lo", "p")); // it replaces only first letter
+console.log(str3.replaceAll("lo", "p")); // it replaces all
+
+console.log(str.charAt(4));
