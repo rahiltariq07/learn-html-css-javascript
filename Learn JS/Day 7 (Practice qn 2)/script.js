d08f04c4589f66d3384d643d6661eb11d4e6e6cc
rahiltariq07 <rahiltariq07@gmail.com>
2024-04-28 18:45:51 +0530

    day-7

--- Learn JS/Day 7 (Practice qn 2)/script.js
+++ Learn JS/Day 7 (Practice qn 2)/script.js
@@ -1,6 +1,7 @@
 /* Qn.2 :- For a given Array with prices of 5 items :- [250, 645, 300, 900, 50]
            All items have an offer of 10% OFF on them. Change the array to store final price after applying offer*/
 
+           
 let items = [250, 645, 300, 900, 50];
 
 for (let i = 0; i < items.length; i++) {
