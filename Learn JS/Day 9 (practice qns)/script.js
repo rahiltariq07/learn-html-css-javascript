d7717d172aa5f8225f66eeb1f077af29f51c6ce0
rahiltariq07 <rahiltariq07@gmail.com>
2024-04-30 11:49:46 +0530

    day-9

---
+++ Learn JS/Day 9 (practice qns)/script.js
@@ -0,0 +1,15 @@
+// For a given array of numbers, print the square of each value using the forEach Loop.
+
+// Rahil's way
+let arr = [1,2,3,4,5,6,7,8,9,10];
+
+arr.forEach((val) => {
+    console.log("square of", val, "=", val**2);
+});
+
+// Mam's Way
+let nums = [2, 3, 4, 5, 6];
+
+nums.forEach((num) => {
+    console.log(num*num);
+});
