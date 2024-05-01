3f2899efef9b240dc384938feaebffa4ba6a3802
rahiltariq07 <rahiltariq07@gmail.com>
2024-05-01 10:42:30 +0530

    day-10

---
+++ Learn JS/Day 10 (part I)/script.js
@@ -0,0 +1,12 @@
+// DOM
+
+// console.log(window);
+
+// console.log(window.document);
+// console.dir(window.document);
+
+// console.log(document);
+// console.dir(document);
+
+console.log(document.body);
+console.dir(document.body);
