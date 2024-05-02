7f63204de2d277b4f65cf8a493b6a7f4872b259a
rahiltariq07 <rahiltariq07@gmail.com>
2024-05-02 18:34:53 +0530

    day-11

---
+++ Learn JS/Day 11 (part I)/script.js
@@ -0,0 +1,15 @@
+// DOM Attributes
+// let div = document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let para = document.querySelector("p");
+// console.log(para.setAttribute("class", "newClass"));
+
+// DOM Style
+let div = document.querySelector("div");
+div.style.backgroundColor = "green";
+div.style.fontSize = "25px";
+div.innerText = "Hello!"
