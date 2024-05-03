aa0d7e014394ef6a33fbfb834e068f8d3bf81f4b
rahiltariq07 <rahiltariq07@gmail.com>
2024-05-03 15:16:21 +0530

    day-11

---
+++ Learn JS/Day 11 (practice qn 02)/script.js
@@ -0,0 +1,10 @@
+/* Qno.02:- Create a <p> tag in HTML, give it a class & some styling.
+Now create a new class in CSS and try to append this class to the <p> element.
+
+Did you notice, how you overwrite the class name when you add a new one?
+Solve this problem using classList.*/
+
+let para = document.querySelector("p");
+para.classList.add("newClass");
+
+console.log(para.classList);
